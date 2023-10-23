4227c8222bfd657e111034bbccd342d11e8c752d
Rafael Maltez <rafaelmaltez89@gmail.com>
2023-10-23 12:58:01 -0300

    if else statment

---
+++ IF-ELSE.cbl
@@ -0,0 +1,25 @@
+      ******************************************************************
+      * Author: Rafael Maltez
+      * Date: 21/10/2023
+      * Purpose: Understand if else statments in COBOL
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. IF-ELSE.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       01 WS-AGE PIC 9(2) VALUE 17.
+       01 WS-CAN-DRIVE PIC X(3).
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Enter your age: "
+           ACCEPT WS-AGE
+           IF WS-AGE < 18
+               MOVE "NO" TO WS-CAN-DRIVE
+           ELSE
+               MOVE "YES" TO WS-CAN-DRIVE
+           END-IF
+           DISPLAY "Can drive? " WS-CAN-DRIVE
+            STOP RUN.
+       END PROGRAM IF-ELSE.
